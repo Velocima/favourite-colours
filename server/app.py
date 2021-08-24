5b145dd0e094fa791f8ac46559aec1fa83548005
Velo <hartley.max@outlook.com>
2021-08-24 14:43:30 +0100

    stub endpooints

--- server/app.py
+++ server/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
   return "Hello world!"
+
+@app.route('/colours', methods=["POST", "GET"])
+def colours(): 
+  return "colours"
+
+@app.route('/people', methods=["POST", "GET"])
+def people(): 
+  return "people"
+
+@app.route('/people/<int:person_id>')
+def person(person_id): 
+  return f'Person id: {person_id}'
