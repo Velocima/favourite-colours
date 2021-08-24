b5f9af7bf14f8320da0a1723ae53287263547da9
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-24 21:38:56 +0100

    tests

--- server/test_app.py
+++ server/test_app.py
@@ -11,12 +11,6 @@ class TestAPICase:
         assert res.status == '200 OK'
         assert len(finished["people"]) == 3
 
-    def test_response_length(self, api):
-        res = api.get('/people')
-        finished = res.json
-        assert res.status == '200 OK'
-        assert len(finished["people"]) == 3
-
     def test_route_not_found(self, api):
         res = api.get('/imnothere')
         assert res.status == '404 NOT FOUND'
